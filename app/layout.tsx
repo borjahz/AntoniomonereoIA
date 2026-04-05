d3c8994ca40a19a77a166023e4a9779d02fedfa6
borjahz <borja.h.z.11@gmail.com>
2026-04-05 12:46:38 +0200

    cncnc

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ import { LanguageProvider } from '@/contexts/LanguageContext'
 export const metadata: Metadata = {
   title: 'Antonio Monereo',
   description: 'Antonio Monereo',
+  icons: {
+    icon: '/images/PHOTO-2026-04-05-12-42-53.jpg',
+    apple: '/images/PHOTO-2026-04-05-12-42-53.jpg',
+  },
   keywords: [
     'Antonio Monereo',
     'artista visual',
